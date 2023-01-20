52083111eff068aac5acde7cea5655751231a00d
HugoDemont <hugodemont62@hotmail.fr>
2023-01-20 09:26:44 +0100

    Hugo D 20/01/23

--- app/src/main/java/com/example/projecty/MainActivity.kt
+++ app/src/main/java/com/example/projecty/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.projecty
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.chargin)
+
+            val handler = Handler(Looper.getMainLooper())
+            handler.postDelayed({
+                setContentView(R.layout.activity_main)
+            }, 3000)
+        }
 }
