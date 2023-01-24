ab30665d1a8fad6d74806e1888fe0e695816debe
HugoDemont <hugodemont62@hotmail.fr>
2023-01-24 12:47:01 +0100

    Hugo D 24/01/23

--- app/src/main/java/com/example/projecty/MainActivity.kt
+++ app/src/main/java/com/example/projecty/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.example.projecty
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Button
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN, //Set status bar to false
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setContentView(R.layout.activity_chargin)
@@ -21,7 +25,15 @@ class MainActivity : AppCompatActivity() {
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
             setContentView(R.layout.activity_main)
+
+
+            val btnExit: Button = findViewById(R.id.exitBtnId)                                          //Selection du BTN pour exit
+            btnExit.setOnClickListener {
+                finish()                                                                               // Fermer l'activité
+            }
         }, 4000)
 
+
     }
+
 }
